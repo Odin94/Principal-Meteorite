e042b542ddc9bda368d363e8bfdec3c2220913e1
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2021-12-12 23:38:18 +0100

    add direction-changing enemy

---
+++ Prefabs/Crab.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+var velocity := Vector2(0, 0)
+const speed = 150
+const gravity = 45
+const jump_force = -900
+
+export var direction = -1
+
+func _ready():
+    if direction == 1:
+        $AnimatedSprite.flip_h = true
+
+func _physics_process(_delta):
+    if is_on_wall():
+        change_direction()
+    
+    velocity.x = speed * direction
+    
+    velocity.y += gravity
+    velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func change_direction():
+    direction *= -1
+    $AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
