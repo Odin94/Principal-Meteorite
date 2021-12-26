46240ecaf977aea1af5f20e019fb30a6686b395b
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2021-12-26 19:52:33 +0100

    allow picking next level that doors lead to

--- Prefabs/Environment/Door.gd
+++ Prefabs/Environment/Door.gd
@@ -3,11 +3,12 @@ extends Node2D
 
 var open = false
 
+export(String, FILE, "*.tscn") var next_level = "res://Levels/Level1.tscn"
 
 func _on_EnterDoorArea_body_entered(body: Node2D):
     if body.name == "Player" and open:
         #warning-ignore:return_value_discarded
-        get_tree().reload_current_scene()
+        get_tree().change_scene(next_level)
 
 
 func _on_OpenDoorArea_area_entered(area: Area2D):
