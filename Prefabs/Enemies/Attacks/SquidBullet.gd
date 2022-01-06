3aca7835f3334ab1058f39ee50fe7eb8ba3513aa
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2022-01-06 23:31:02 +0100

    build basic boss arena and basic first phase

---
+++ Prefabs/Enemies/Attacks/SquidBullet.gd
@@ -0,0 +1,56 @@
+extends Area2D
+
+
+const speed = 15
+
+var direction := Vector2(0, 0)
+var velocity := Vector2(0, 0)
+var damage := 10
+
+var impacting = false
+
+func _physics_process(_delta):
+    if not impacting:
+        if direction != Vector2(0, 0):
+            velocity = direction * speed
+            
+            global_position += velocity
+
+
+func set_direction(new_direction: Vector2):
+    self.direction = new_direction
+
+
+func limit_lifespan(lifespan: float):
+    $Lifespan.wait_time = lifespan
+    $Lifespan.start()
+    
+
+func set_damage(new_damage: int):
+    self.damage = new_damage
+
+
+func _on_SquidBullet_body_entered(body: Node2D):
+    if body.name == "Player":
+        body.get_hurt(damage, position.x, true)
+        impact()
+        
+    if body is TileMap or body.name == "DoorBlocker":
+        impact()
+
+
+func _on_Lifespan_timeout():
+    impact()
+    
+    
+func hit_enemy():
+    impact()
+    
+
+func impact():
+    print("impact")
+    if not impacting:
+        impacting = true
+        $AnimatedSprite.play("impact")
+        yield($AnimatedSprite, "animation_finished")
+        queue_free()
--- Prefabs/Enemies/Squid.gd
+++ Prefabs/Enemies/Squid.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+export (PackedScene) var SquidBullet
+
 var velocity := Vector2(0, 0)
 const speed = 100
 var gravity = 0
@@ -8,31 +10,54 @@ export var direction = -1
 export var health = 300
 export var damage = 20
 
+var bullet_lifespan = 1
+
+onready var player: KinematicBody2D = get_tree().get_nodes_in_group("Player")[0]
 
 func _ready():
     if direction == 1:
         $AnimatedSprite.flip_h = true
-    $FloorChecker.position.x += $CollisionShape2D.shape.get_extents().x * direction
     $AnimatedSprite.play("sleeping")
 
 func _physics_process(_delta):
     if health <= 0:
         return
+        
+    shoot()
     
     if $HitEffectTimeout.is_stopped():
-        if is_on_floor() and (is_on_wall() or not $FloorChecker.is_colliding()):  # TODO: change direction when hitting a wall? Or have a pre-determined route?
+        if is_on_wall():
             change_direction()
         velocity.x = speed * direction
     
-    velocity.y += gravity
     velocity = move_and_slide(velocity, Vector2.UP)
 
 
+func shoot():
+    if $ShootingCooldown.is_stopped():
+        print("shooting")
+        # $ShotSound.play()
+        var bullet = SquidBullet.instance()
+        owner.add_child(bullet)
+        bullet.transform = global_transform
+        
+        var bullet_offset = $AnimatedSprite.frames.get_frame("_idle", 0).get_width() * 0.9
+        bullet.position.x += bullet_offset if direction == 1 else -bullet_offset
+        
+        bullet.set_damage(damage)
+        # TODO: Aim for center mass, maybe re-open this thread https://godotengine.org/qa/38373/problem-involving-vectors-and-shooting-at-the-player ?
+        bullet.set_direction(position.direction_to(player.global_position))
+        bullet.limit_lifespan(bullet_lifespan)        
+        
+        $ShootingCooldown.start()
+
+
 func change_direction():
-    direction *= -1
-    $FloorChecker.position.x += $CollisionShape2D.shape.get_extents().x * 2 * direction
-    
-    $AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
+    if $TurnAroundCooldown.is_stopped():
+        direction *= -1
+        
+        $AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
+        $TurnAroundCooldown.start()
 
 
 func _on_DamageArea_body_entered(body: Node2D):
@@ -63,9 +88,13 @@ func _on_HitEffectTimeout_timeout():
     set_modulate(Color(1, 1, 1, 1))
     velocity.x = speed * direction
 
+
 func die():
     $DeathSound.play()
     # TODO: Play cool death animation stuff
     set_modulate(Color(1, 1, 1, 1))
     yield($AnimatedSprite, "animation_finished")
     queue_free()
+
+
+
