ab6847337a7f2bc0051b974a955946ffa5fea808
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2022-03-25 14:00:39 +0100

    make health upgrade pulsate

--- Prefabs/Upgrades/HealthUpgrade.gd
+++ Prefabs/Upgrades/HealthUpgrade.gd
@@ -1,8 +1,20 @@
 extends Node2D
 
+const min_scale = 1
+const max_scale = 1.3
+var target_scale = max_scale
+
 func _ready():
     if Globals.collected_health_powerups.has(self.name):
         queue_free()
+
+func _physics_process(_delta):
+    $Area2D/Sprite.scale.x = lerp($Area2D/Sprite.scale.x, target_scale, .03)
+    $Area2D/Sprite.scale.y = lerp($Area2D/Sprite.scale.y, target_scale, .03)
+    if $Area2D/Sprite.scale.x >= (max_scale - 0.05):
+        target_scale = min_scale
+    elif $Area2D/Sprite.scale.x <= (min_scale + 0.05):
+        target_scale = max_scale
 
 func _on_Area2D_body_entered(body: Node2D):
     if body.name == "Player":
