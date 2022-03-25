a761413f813c1e663f32502550ea43e35e7a0864
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2022-03-25 14:13:26 +0100

    make jump upgrade float up and down

--- Prefabs/Upgrades/JumpUpgrade.gd
+++ Prefabs/Upgrades/JumpUpgrade.gd
@@ -1,8 +1,21 @@
 extends Node2D
 
+const min_height = -20
+const max_height = 0
+var target_height = min_height
+
 func _ready():
     if Globals.collected_jump_powerups.has(self.name):
         queue_free()
+
+func _physics_process(_delta):
+    $Area2D/Sprite.position.y = lerp($Area2D/Sprite.position.y, target_height, .03)
+    if $Area2D/Sprite.position.y >= (max_height - 0.5):
+        target_height = min_height
+    elif $Area2D/Sprite.position.y <= (min_height + 0.5):
+        target_height = max_height
+    print($Area2D/Sprite.position.y)
+    print(target_height)
 
 func _on_Area2D_body_entered(body: Node2D):
     if body.name == "Player":
