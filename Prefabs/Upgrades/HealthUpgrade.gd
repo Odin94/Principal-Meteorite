31bb861e89af5869820e98400951f7c2f190760a
Odin Kammerloher <kammerloher.andreas@gmail.com>
2022-09-10 19:34:55 +0200

    add level 7

--- Prefabs/Upgrades/HealthUpgrade.gd
+++ Prefabs/Upgrades/HealthUpgrade.gd
@@ -6,28 +6,28 @@ const scaling_speed = .03
 var target_scale = max_scale
 
 func _ready():
-    if Globals.collected_health_powerups.has(self.name):
-        queue_free()
+	if Globals.collected_health_powerups.has(self.name):
+		queue_free()
 
 func _physics_process(_delta):
-    $Area2D/Sprite.scale.x = lerp($Area2D/Sprite.scale.x, target_scale, scaling_speed)
-    $Area2D/Sprite.scale.y = lerp($Area2D/Sprite.scale.y, target_scale, scaling_speed)
-    if $Area2D/Sprite.scale.x >= (max_scale - 0.05):
-        target_scale = min_scale
-    elif $Area2D/Sprite.scale.x <= (min_scale + 0.05):
-        target_scale = max_scale
+	$Area2D/Sprite.scale.x = lerp($Area2D/Sprite.scale.x, target_scale, scaling_speed)
+	$Area2D/Sprite.scale.y = lerp($Area2D/Sprite.scale.y, target_scale, scaling_speed)
+	if $Area2D/Sprite.scale.x >= (max_scale - 0.05):
+		target_scale = min_scale
+	elif $Area2D/Sprite.scale.x <= (min_scale + 0.05):
+		target_scale = max_scale
 
 func _on_Area2D_body_entered(body: Node2D):
-    if body.name == "Player":
-        body.get_health_upgrade()
-        print("Found health upgrade!")
-        $PauseTimer.start()
-        z_index = 5
-        $PickUpSound.play()
-        $PickUpText.visible = true
-        get_tree().paused = true
-        Globals.collected_health_powerups.append(self.name)
+	if body.name == "Player":
+		body.get_health_upgrade()
+		print("Found health upgrade!")
+		$PauseTimer.start()
+		z_index = 5
+		$PickUpSound.play()
+		$PickUpText.visible = true
+		get_tree().paused = true
+		Globals.collected_health_powerups.append(self.name)
 
 func _on_PauseTimer_timeout():
-    get_tree().paused = false
-    queue_free()
+	get_tree().paused = false
+	queue_free()
