6ef348db16fd4f38b259d0c277e656c2814b0b91
Odin Kammerloher <kammerloher.andreas@gmail.com>
2022-09-10 19:12:52 +0200

    complete level 6

--- Prefabs/Enemies/Jumper.gd
+++ Prefabs/Enemies/Jumper.gd
@@ -18,85 +18,85 @@ onready var player: KinematicBody2D = get_tree().get_nodes_in_group("Player")[0]
 var dead = false
 
 func _ready():
-    $AnimatedSprite.play("_idle")
-    rng.randomize()
+	$AnimatedSprite.play("_idle")
+	rng.randomize()
 
 func _physics_process(_delta):
-    if dead:
-        return
+	if dead:
+		return
 
-    if $HitEffectTimeout.is_stopped():
-        if is_on_floor():
-            jump()
-    
-    if not is_on_floor():
-        velocity.x = speed * direction
-    else:
-        velocity.x = 0
-        $AnimatedSprite.play("_idle")
-    
-    velocity.y += gravity
-    velocity = move_and_slide(velocity, Vector2.UP)
+	if $HitEffectTimeout.is_stopped():
+		if is_on_floor():
+			jump()
+	
+	if not is_on_floor():
+		velocity.x = speed * direction
+	else:
+		velocity.x = 0
+		$AnimatedSprite.play("_idle")
+	
+	velocity.y += gravity
+	velocity = move_and_slide(velocity, Vector2.UP)
 
 
 func jump():
-    if $JumpingCooldown.is_stopped() and position.distance_to(player.position) < trigger_distance:  # and idle animation at last frame?
-        var x_pos_delta = player.position.x - position.x
-        var abs_pos_delta = abs(x_pos_delta)
-        if abs_pos_delta == 0:
-            abs_pos_delta += 1
-        direction = x_pos_delta / abs_pos_delta
-        
-        if position.distance_to(player.position) < 180:
-            velocity.y = jump_force / 2
-        else:
-            velocity.y = jump_force
-        
-        $AnimatedSprite.play("jumping")
-        $JumpingCooldown.start()
+	if $JumpingCooldown.is_stopped() and position.distance_to(player.position) < trigger_distance:  # and idle animation at last frame?
+		var x_pos_delta = player.position.x - position.x
+		var abs_pos_delta = abs(x_pos_delta)
+		if abs_pos_delta == 0:
+			abs_pos_delta += 1
+		direction = x_pos_delta / abs_pos_delta
+		
+		if position.distance_to(player.position) < 180:
+			velocity.y = jump_force / 2
+		else:
+			velocity.y = jump_force
+		
+		$AnimatedSprite.play("jumping")
+		$JumpingCooldown.start()
 
 
 func _on_DamageArea_body_entered(body: Node2D):
-    if dead:
-        return
+	if dead:
+		return
 
-    if body.name == "Player":
-        body.get_hurt(damage, position.x, true)
+	if body.name == "Player":
+		body.get_hurt(damage, position.x, true)
 
 
 func _on_DamageArea_area_entered(area: Area2D):
-    if area.name.validate_node_name().begins_with("Bullet"):
-        get_hurt(area.damage)
-        area.hit_enemy()
+	if area.name.validate_node_name().begins_with("Bullet"):
+		get_hurt(area.damage)
+		area.hit_enemy()
 
 
 func get_hurt(incoming_damage: int):
-    $HurtSound.play()
-    set_modulate(Color(1, 0.3, 0.3, 0.3))
-    velocity.x = 0
-    $HitEffectTimeout.start(.1)
-    health -= incoming_damage
-    if health <= 0:
-        die()
+	$HurtSound.play()
+	set_modulate(Color(1, 0.3, 0.3, 0.3))
+	velocity.x = 0
+	$HitEffectTimeout.start(.1)
+	health -= incoming_damage
+	if health <= 0:
+		die()
 
 
 func _on_HitEffectTimeout_timeout():
-    set_modulate(Color(1, 1, 1, 1))
-    velocity.x = speed * direction
+	set_modulate(Color(1, 1, 1, 1))
+	velocity.x = speed * direction
 
 func die():
-    if dead:
-        return
-        
-    dead = true
-    
-    $DeathSound.play()
-    $AnimatedSprite.play("death")
-    set_modulate(Color(1, 1, 1, 1))
-    yield($AnimatedSprite, "animation_finished")
-    
-    if rng.randf_range(0, 10.0) > 5:
-        var health_drop = HealthDrop.instance()
-        owner.add_child(health_drop)
-        health_drop.transform = global_transform
-    queue_free()
+	if dead:
+		return
+		
+	dead = true
+	
+	$DeathSound.play()
+	$AnimatedSprite.play("death")
+	set_modulate(Color(1, 1, 1, 1))
+	yield($AnimatedSprite, "animation_finished")
+	
+	if rng.randf_range(0, 10.0) > 5:
+		var health_drop = HealthDrop.instance()
+		owner.add_child(health_drop)
+		health_drop.transform = global_transform
+	queue_free()
--- Prefabs/Environment/Door.gd
+++ Prefabs/Environment/Door.gd
@@ -6,16 +6,16 @@ var open = false
 export(String, FILE, "*.tscn") var next_level = "res://Levels/Level1.tscn"
 
 func _on_EnterDoorArea_body_entered(body: Node2D):
-    if body.name == "Player" and open:
-        Globals.coming_from_door = name
-        Globals.player_direction = body.direction
-        body.save_stats()
-        #warning-ignore:return_value_discarded
-        get_tree().change_scene(next_level)
+	if body.name == "Player" and open:
+		Globals.coming_from_door = name
+		Globals.player_direction = body.direction
+		body.save_stats()
+		#warning-ignore:return_value_discarded
+		get_tree().change_scene(next_level)
 
 
 func _on_OpenDoorArea_area_entered(area: Area2D):
-    if area.name == "Bullet" and not open:
-        $AnimatedSprite.play("opening")
-        $DoorBlocker.queue_free()
-        open = true
+	if area.name == "Bullet" and not open:
+		$AnimatedSprite.play("opening")
+		$DoorBlocker.queue_free()
+		open = true
